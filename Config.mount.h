d4027edfdcfe50529cf0ba08ca3cf542a73f44b7
Howard Dutton <hjd1964@gmail.com>
2021-06-28 19:53:35 -0400

    Updated mount config for new options

--- Config.mount.h
+++ Config.mount.h
@@ -80,17 +80,17 @@
 #define MOUNT_TYPE                    GEM //    GEM, GEM for German Equatorial, FORK for Equatorial Fork, or ALTAZM          <-Req'd
                                           //         Dobsonian etc. mounts. GEM Eq mounts perform meridian flips.
 
-// USER FEEDBACK -------------------------------------------- see https://onstep.groups.io/g/main/wiki/6-Configuration#USER_FEEDBACK
-#define LED_MOUNT_STATUS              OFF //    OFF, ON Flashes proportional to movement rate (2Hz=1x sidereal) on for slews. Option
-#define BUZZER                        OFF //    OFF, ON, n. Where n=100..6000 (Hz freq.) for speaker. ON for piezo buzzer.    Option
-#define BUZZER_DEFAULT                OFF //    OFF, ON default starts w/buzzer enabled.                                      Option
-#define BUZZER_MEMORY                 OFF //    OFF, ON to remember buzzer setting across power cycles.                       Option
-
 // TIME AND LOCATION -------------------------------------------------- see https://onstep.groups.io/g/main/wiki/6-Configuration#TLS
 #define TIME_LOCATION_SOURCE          OFF //    OFF, DS3231 (I2c,) DS3234 (Spi,) TEENSY (T3.2 internal,) or GPS source.       Option
                                           //         Provides Date/Time, and if available, PPS & Lat/Long also.
-#define TIME_LOCATION_PPS_SENSE       OFF //    OFF, ON* enables PPS (pulse per second,) senses signal rising edge.           Option
+#define TIME_LOCATION_PPS_SENSE       OFF //    OFF, ON enables PPS (pulse per second,) senses signal rising edge.            Option
                                           //         Better tracking accuracy especially for Mega2560's w/ceramic resonator.
+
+// USER FEEDBACK -------------------------------------------- see https://onstep.groups.io/g/main/wiki/6-Configuration#USER_FEEDBACK
+#define LED_MOUNT_STATUS               ON //    OFF, ON Flashes proportional to rate of movement or solid on for slews.       Option
+#define BUZZER                        OFF //    OFF, ON, n. Where n=100..6000 (Hz freq.) for speaker. ON for piezo buzzer.    Option
+#define BUZZER_DEFAULT                OFF //    OFF, ON default starts w/buzzer enabled.                                      Option
+#define BUZZER_MEMORY                 OFF //    OFF, ON to remember buzzer setting across power cycles.                       Option
 
 // SENSORS -------------------------------------------------------- see https://onstep.groups.io/g/main/wiki/6-Configuration#SENSORS
 #define LIMIT_SENSE                   OFF //    OFF, HIGH or LOW state on limit sense switch stops movement.                  Option
