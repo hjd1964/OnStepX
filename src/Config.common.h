eb09c8aacdba0add44e563d8320a2eeefbd94a4f
Howard Dutton <hjd1964@gmail.com>
2021-07-09 09:05:15 -0400

    Bring in the rotator configuration file

--- src/Config.common.h
+++ src/Config.common.h
@@ -4,6 +4,7 @@
 
 #include "../Config.controller.h"
 #include "../Config.mount.h"
+#include "../Config.rotator.h"
 #include "../Config.focusers.h"
 #include "../Config.features.h"
 #include "../Config.controllerX.h"
