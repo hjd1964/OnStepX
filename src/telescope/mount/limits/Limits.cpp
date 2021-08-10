d27c14dc6478ee61eea7815699d3be562c3f507d
Howard Dutton <hjd1964@gmail.com>
2021-08-10 11:23:39 -0400

    Better error priority order

--- src/telescope/mount/limits/Limits.cpp
+++ src/telescope/mount/limits/Limits.cpp
@@ -93,10 +93,9 @@ uint8_t Limits::errorCode() {
   ERR_ALT_MAX, ERR_WEATHER_INIT, ERR_SITE_INIT, ERR_NV_INIT};
 
   // priority highest to lowest
+  if (error.motorFault) return (uint8_t)ERR_MOTOR_FAULT;
   if (error.limitSense.axis1.min || error.limitSense.axis1.max ||
       error.limitSense.axis2.min || error.limitSense.axis2.max) return (uint8_t)ERR_LIMIT_SENSE;
-  if (error.motorFault) return (uint8_t)ERR_MOTOR_FAULT;
-  if (initError.nv || initError.value) return (uint8_t)ERR_NV_INIT;
   if (error.altitude.min) return (uint8_t)ERR_ALT_MIN;
   if (error.altitude.max) return (uint8_t)ERR_ALT_MAX;
   if (transform.mountType == ALTAZM) {
@@ -108,6 +107,7 @@ uint8_t Limits::errorCode() {
     if (error.limit.axis2.min || error.limit.axis2.max) return (uint8_t)ERR_DEC;
   }
   if (error.meridian.east || error.meridian.west) return (uint8_t)ERR_MERIDIAN;
+  if (initError.nv || initError.value) return (uint8_t)ERR_NV_INIT;
   if (initError.tls) return (uint8_t)ERR_SITE_INIT;
   if (initError.weather) return (uint8_t)ERR_WEATHER_INIT;
   return ERR_NONE;
