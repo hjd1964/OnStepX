5ab553e8c9241b64e982bf566872f0c9e41f94e0
Howard Dutton <hjd1964@gmail.com>
2021-09-13 19:31:49 -0400

    Better debug message

--- src/telescope/mount/site/Site.cpp
+++ src/telescope/mount/site/Site.cpp
@@ -74,7 +74,7 @@ void Site::init() {
 
   setSiderealTime(ut1);
 
-  VF("MSG: Mount, site start fracsec timer task (rate 10ms priority 0)... ");
+  VF("MSG: Mount, site start sidereal timer task (rate 10ms priority 0)... ");
   delay(1000);
   // period ms (0=idle), duration ms (0=forever), repeat, priority (highest 0..7 lowest), task_handle
   handle = tasks.add(0, 0, true, 0, clockTickWrapper, "ClkTick");
