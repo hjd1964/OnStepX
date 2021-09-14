c890ba42b2e8428f88a93b2d0c8b816ce8dcc3eb
Howard Dutton <hjd1964@gmail.com>
2021-09-14 15:24:53 -0400

    Fastest allowed is 1ms

--- src/HAL/HAL_Teensy_4.0.h
+++ src/HAL/HAL_Teensy_4.0.h
@@ -7,8 +7,8 @@
 // This platform has digitalReadFast, digitalWriteFast, etc.
 #define HAL_HAS_DIGITAL_FAST
 
-// 1/2000 second sidereal timer
-#define HAL_SIDEREAL_FRAC 2000.0
+// 1/1000 second sidereal timer
+#define HAL_SIDEREAL_FRAC 1000.0
 
 // This platform has 15 bit PWM
 #ifndef HAL_ANALOG_WRITE_BITS
