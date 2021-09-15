8e39f1b26b9fc2ee94fe165da2413f58d02df6b9
Howard Dutton <hjd1964@gmail.com>
2021-09-15 16:09:34 -0400

    Allow more range for BL takeup rate

--- Extended.config.h
+++ Extended.config.h
@@ -126,7 +126,7 @@
 // TRACKING BEHAVIOUR -------------------------------------------- see https://onstep.groups.io/g/main/wiki/6-Configuration#TRACKING
 #define TRACK_AUTOSTART               OFF //    OFF, ON Start with tracking enabled.                                          Option
 #define TRACK_REFRACTION_RATE_DEFAULT OFF //    OFF, ON Start w/atmospheric refract. compensation (RA axis/Eq mounts only.)   Option
-#define TRACK_BACKLASH_RATE            25 //     25, n. Where n=2..50 (x sidereal rate) during backlash takeup.               Option
+#define TRACK_BACKLASH_RATE            25 //     25, n. Where n=2..100 (x sidereal rate) during backlash takeup.              Option
                                           //         Too fast motors stall/gears slam or too slow and sluggish in backlash.
 
 // SLEWING BEHAVIOUR ---------------------------------------------- see https://onstep.groups.io/g/main/wiki/6-Configuration#SLEWING
