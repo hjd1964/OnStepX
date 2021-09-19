c7d5eb5314b1bbfac36925a91f2cdb661d327d37
Howard Dutton <hjd1964@gmail.com>
2021-09-19 07:27:38 -0400

    Changed firmware name to On-Step

--- OnStepX.ino
+++ OnStepX.ino
@@ -41,7 +41,7 @@
 // Use tabs "Config..." to configure OnStep to your requirements
 
 // Firmware version ----------------------------------------------------------------------------------------------------------------
-#define FirmwareName                "OnStepX"
+#define FirmwareName                "On-Step"
 #define FirmwareVersionMajor        10
 #define FirmwareVersionMinor        03     // minor version 00 to 99
 #define FirmwareVersionPatch        "h"    // for example major.minor patch: 10.03c
