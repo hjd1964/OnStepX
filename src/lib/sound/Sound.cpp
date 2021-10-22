21570cb1bfb7a63cc537c084bcd47ca2eed5c16a
Howard Dutton <hjd1964@gmail.com>
2021-10-22 10:36:21 -0400

    Fix for sound tasks not being disposed of properly

--- src/lib/sound/Sound.cpp
+++ src/lib/sound/Sound.cpp
@@ -24,7 +24,7 @@ void Sound::alert() {
   if (enabled) {
     #if STATUS_BUZZER == ON
       digitalWriteEx(STATUS_BUZZER_PIN, STATUS_BUZZER_ON_STATE);
-      tasks.add(100, 0, false, 6, buzzerOff);
+      tasks.add(100, 200, false, 6, buzzerOff);
     #endif
     #if STATUS_BUZZER >= 0
       tone(STATUS_BUZZER_PIN, STATUS_BUZZER, 1000);
@@ -36,7 +36,7 @@ void Sound::beep() {
   if (enabled) {
     #if STATUS_BUZZER == ON
       digitalWriteEx(STATUS_BUZZER_PIN, STATUS_BUZZER_ON_STATE);
-      tasks.add(25, 0, false, 6, buzzerOff);
+      tasks.add(25, 50, false, 6, buzzerOff);
     #endif
     #if STATUS_BUZZER >= 0
       tone(STATUS_BUZZER_PIN, STATUS_BUZZER, 250);
@@ -48,7 +48,7 @@ void Sound::click() {
   if (enabled) {
     #if STATUS_BUZZER == ON
       digitalWriteEx(STATUS_BUZZER_PIN, STATUS_BUZZER_ON_STATE);
-      tasks.add(5, 0, false, 6, buzzerOff);
+      tasks.add(5, 10, false, 6, buzzerOff);
     #endif
     #if STATUS_BUZZER >= 0
       tone(STATUS_BUZZER_PIN, STATUS_BUZZER, 50);
