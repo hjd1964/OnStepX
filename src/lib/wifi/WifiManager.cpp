16ec91d4002d3fcd903087bfe64bb7d70afb7e34
Howard Dutton <hjd1964@gmail.com>
2021-10-23 09:38:37 -0400

    Better debug messages

--- src/lib/wifi/WifiManager.cpp
+++ src/lib/wifi/WifiManager.cpp
@@ -27,16 +27,17 @@ void WifiManager::init() {
 
     VF("MSG: WiFi Master Pwd  = "); VL(settings.masterPassword);
 
-    VF("MSG: WiFi AP Enabled  = "); VL(settings.accessPointEnabled);
     VF("MSG: WiFi Sta Enabled = "); VL(settings.stationEnabled);
     VF("MSG: WiFi Sta DHCP En = "); VL(settings.stationDhcpEnabled);
 
-    VF("MSG: WiFi STA SSID    = "); VL(settings.sta_ssid);
-    VF("MSG: WiFi STA PWD     = "); VL(settings.sta_pwd);
+    VF("MSG: WiFi Sta SSID    = "); VL(settings.sta_ssid);
+    VF("MSG: WiFi Sta PWD     = "); VL(settings.sta_pwd);
 
-    VF("MSG: WiFi STA IP      = "); VL(sta_ip.toString());
-    VF("MSG: WiFi STA GATEWAY = "); VL(sta_gw.toString());
-    VF("MSG: WiFi STA SN      = "); VL(sta_sn.toString());
+    VF("MSG: WiFi Sta IP      = "); VL(sta_ip.toString());
+    VF("MSG: WiFi Sta GATEWAY = "); VL(sta_gw.toString());
+    VF("MSG: WiFi Sta SN      = "); VL(sta_sn.toString());
+
+    VF("MSG: WiFi AP Enabled  = "); VL(settings.accessPointEnabled);
 
     VF("MSG: WiFi AP SSID     = "); VL(settings.ap_ssid);
     VF("MSG: WiFi AP PWD      = "); VL(settings.ap_pwd);
@@ -48,19 +49,19 @@ void WifiManager::init() {
 
   TryAgain:
     if (settings.accessPointEnabled && !settings.stationEnabled) {
-      VLF("MSG: WiFi Starting Soft AP");
+      VLF("MSG: WiFi starting Soft AP");
       WiFi.softAP(settings.ap_ssid, settings.ap_pwd, settings.ap_ch);
       WiFi.mode(WIFI_AP);
     } else
     if (!settings.accessPointEnabled && settings.stationEnabled) {
-      VLF("MSG: WiFi Starting Station");
+      VLF("MSG: WiFi starting Station");
       WiFi.begin(settings.sta_ssid, settings.sta_pwd);
       WiFi.mode(WIFI_STA);
     } else
     if (settings.accessPointEnabled && settings.stationEnabled) {
-      VLF("MSG: WiFi Starting Soft AP");
+      VLF("MSG: WiFi starting Soft AP");
       WiFi.softAP(settings.ap_ssid, settings.ap_pwd, settings.ap_ch);
-      VLF("MSG: WiFi Starting Station");
+      VLF("MSG: WiFi starting Station");
       WiFi.begin(settings.sta_ssid, settings.sta_pwd);
       WiFi.mode(WIFI_AP_STA);
     }
@@ -74,9 +75,9 @@ void WifiManager::init() {
     if (!settings.accessPointEnabled && settings.stationEnabled) {
       for (int i = 0; i < 8; i++) if (WiFi.status() != WL_CONNECTED) delay(1000); else break;
       if (WiFi.status() != WL_CONNECTED) {
-        VLF("MSG: WiFi Starting Station, failed");
+        VLF("MSG: WiFi starting station failed");
         WiFi.disconnect(); delay(3000);
-        VLF("MSG: WiFi Switching to Soft AP mode");
+        VLF("MSG: WiFi switching to Soft AP mode");
         settings.stationEnabled = false;
         settings.accessPointEnabled = true;
         goto TryAgain;
