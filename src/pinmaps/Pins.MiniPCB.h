736d65123b92383d2aa854814cea4250e6b97693
Howard Dutton <hjd1964@gmail.com>
2022-03-14 16:04:42 -0400

    Added TMC2209U support

--- src/pinmaps/Pins.MiniPCB.h
+++ src/pinmaps/Pins.MiniPCB.h
@@ -22,6 +22,11 @@
   #define SERIAL_E_BAUD_DEFAULT 9600
 #endif
 
+// Use the following settings for any TMC UART driver (TMC2209) that may be present
+#define SERIAL_TMC              SoftSerial  // Use software serial with RX on M2 and TX on M3 of axis
+#define SERIAL_TMC_BAUD         230400      // Baud rate
+#define SERIAL_TMC_NO_RX                    // Recieving data doesn't work with software serial
+
 // The multi-purpose pins (Aux3..Aux8 can be analog pwm/dac if supported)
 #define AUX0_PIN                19               // Status LED
 #define AUX1_PIN                18               // ESP8266 GPIO0, SPI MISO/Fault
--- src/pinmaps/Pins.STM32B.h
+++ src/pinmaps/Pins.STM32B.h
@@ -28,6 +28,11 @@
   #error "Configuration (Config.h): SerialC isn't supported, disable this option."
 #endif
 
+// Use the following settings for any TMC UART driver (TMC2209) that may be present
+#define SERIAL_TMC              SoftSerial  // Use software serial with RX on M2 and TX on M3 of axis
+#define SERIAL_TMC_BAUD         230400      // Baud rate
+#define SERIAL_TMC_NO_RX                    // Recieving data doesn't work with software serial
+
 // === Pins for DS3231 RTC/EEPROM
 // The STM32 has no built in EEPROM. Therefore, we use a DS3231 RTC module 
 // which has an EEPROM chip on the board. HAL takes care of its address and size:
