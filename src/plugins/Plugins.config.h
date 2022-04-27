9704bede415f607dabf462e1b1023ddd3e364ca3
Howard Dutton <hjd1964@gmail.com>
2022-04-27 05:56:23 -0400

    Added support for plugins

---
+++ src/plugins/Plugins.config.h
@@ -0,0 +1,34 @@
+/* ---------------------------------------------------------------------------------------------------------------------------------
+ * User plugins for OnStepX
+ * ---------------------------------------------------------------------------------------------------------------------------------
+*/
+
+// =================================================================================================================================
+
+#define PLUGIN1                       OFF //    OFF, Specify the class void init(); method to call to enable.                 Option
+//#include "plugin1/Name.h"               //         Specify the header file to include the class.
+
+// *** Comment out PLUGIN1 above AND uncomment the following two lines to enable the sample plugin ***
+//#define PLUGIN1                    sample //    OFF, Specify the class void init(); method to call to enable.                 Option
+//#include "sample/Sample.h"                //         Specify the header file to include the class.
+
+#define PLUGIN2                       OFF //    OFF, Specify the class void init(); method to call to enable.                 Option
+//#include "plugin2/Name.h"               //         Specify the header file to include the class.
+
+#define PLUGIN3                       OFF //    OFF, Specify the class void init(); method to call to enable.                 Option
+//#include "plugin3/Name.h"               //         Specify the header file to include the class.
+
+#define PLUGIN4                       OFF //    OFF, Specify the class void init(); method to call to enable.                 Option
+//#include "plugin3/Name.h"               //         Specify the header file to include the class.
+
+#define PLUGIN5                       OFF //    OFF, Specify the class void init(); method to call to enable.                 Option
+//#include "plugin3/Name.h"               //         Specify the header file to include the class.
+
+#define PLUGIN6                       OFF //    OFF, Specify the class void init(); method to call to enable.                 Option
+//#include "plugin3/Name.h"               //         Specify the header file to include the class.
+
+#define PLUGIN7                       OFF //    OFF, Specify the class void init(); method to call to enable.                 Option
+//#include "plugin3/Name.h"               //         Specify the header file to include the class.
+
+#define PLUGIN8                       OFF //    OFF, Specify the class void init(); method to call to enable.                 Option
+//#include "plugin3/Name.h"               //         Specify the header file to include the class.
