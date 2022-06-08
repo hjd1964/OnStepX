b446a10193c5a3eaf247f8f81fcc6856f73e843a
Howard Dutton <hjd1964@gmail.com>
2022-06-08 06:28:29 -0400

    Corrected ODrive timeout

--- src/lib/axis/motor/oDrive/ODrive.cpp
+++ src/lib/axis/motor/oDrive/ODrive.cpp
@@ -108,11 +108,8 @@ bool ODriveMotor::validateParameters(float param1, float param2, float param3, f
 // sets motor power on/off (if possible)
 void ODriveMotor::power(bool state) {
   int requestedState = AXIS_STATE_IDLE;
-  float timeout = 0.01;
-  if (state) {
-    requestedState = AXIS_STATE_CLOSED_LOOP_CONTROL;
-    timeout = 0.5;
-  }
+  if (state) requestedState = AXIS_STATE_CLOSED_LOOP_CONTROL;
+  float timeout = 0.5;
   if(!_oDriveDriver->run_state(axisNumber - 1, requestedState, false, timeout)) {
     VF("WRN: ODrive"); V(axisNumber); VF(", ");
     VLF("closed loop control - command timeout!");
