31339249681d3d6df3ff1d13f859db26ca575fb2
Howard Dutton <hjd1964@gmail.com>
2022-12-12 13:21:57 -0500

    Added default for webserver

--- Extended.config.h
+++ Extended.config.h
@@ -37,6 +37,7 @@
 #define SERIAL_BT_MODE                OFF //    OFF, Use SLAVE to enable the interface (ESP32 only.)                          Option
 #define SERIAL_BT_NAME          "OnStepX" //         "OnStepX", Bluetooth device name.                                        Adjust
 #define SERIAL_IP_MODE                OFF //    OFF, WIFI_ACCESS_POINT or WIFI_STATION enables interface (ESP32 only.)        Option
+#define WEB_SERVER                    OFF //    OFF, ON enables Webserver (for Website plugin)                                Option
 
 // EXTERNAL GPIO SUPPORT -----------------------------------------------------------------------------------------------------------
 #define GPIO_DEVICE                   OFF //    OFF, DS2413: for 2-ch or 4-ch using 1-wire gpio's (one or two devices.)       Option
