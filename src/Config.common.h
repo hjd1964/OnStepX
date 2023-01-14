170110b1be0743f5df858f438b49c97f7bc2d765
Howard Dutton <hjd1964@gmail.com>
2023-01-14 08:18:12 -0500

    Remove unused file

--- src/Config.common.h
+++
@@ -1,6 +0,0 @@
-// -----------------------------------------------------------------------------------
-// Combined configuration
-#pragma once
-
-#include "../Config.h"
-#include "Config.defaults.h"
