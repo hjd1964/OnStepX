67b46e2f5aaedda6941156c971b538b3679189e1
Howard Dutton <hjd1964@gmail.com>
2023-02-19 11:40:32 -0500

    Added Kalaman filter #define

--- src/lib/Constants.h
+++ src/lib/Constants.h
@@ -24,6 +24,7 @@
 #define STANDARD                    -19
 #define PERSISTENT                  -20
 #define ERRORS_ONLY                 -21
+#define KALAMAN                     -22
 #define INVALID                     -127
 
 // driver (step/dir interface, usually for stepper motors)
