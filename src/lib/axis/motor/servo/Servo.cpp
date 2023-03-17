9c21995a208edd6cd01db2017ef6802dfe5939de
Howard Dutton <hjd1964@gmail.com>
2023-03-17 09:00:45 -0400

    Fixed debug message

--- src/lib/axis/motor/servo/Servo.cpp
+++ src/lib/axis/motor/servo/Servo.cpp
@@ -167,10 +167,8 @@ bool ServoMotor::validateParameters(float param1, float param2, float param3, fl
 
 // sets motor enable on/off (if possible)
 void ServoMotor::enable(bool state) {
-  V(axisPrefix); VF("driver powered ");
-  if (state) { VLF("up"); } else { VLF("down"); } 
-
   if (!state) feedback->reset();
+
   driver->enable(state);
   enabled = state;
 }
