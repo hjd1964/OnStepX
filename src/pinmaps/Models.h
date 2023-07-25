8f66386455b4fd29677421e42ce478cad4340f9d
Michel Moriniaux <MichelMoriniaux@users.noreply.github.com>
2023-07-24 18:18:59 -0700

    Candidate release for JTW OnstepX STM3
    Candidate release for modularized JTW OnstepX on STM32F411 hardware

--- src/pinmaps/Models.h
+++ src/pinmaps/Models.h
@@ -67,6 +67,10 @@
   #define PINMAP_STR "FYSETC S6 v2"
   #include "Pins.FYSETC_S6.h"
 #endif
+#if PINMAP == JTWSTM
+  #define PINMAP_STR "JTW v2.1"
+  #include "Pins.JTWPCB2.h"
+#endif
 #if PINMAP == MaxSTM3 || PINMAP == MaxSTM3I
   #define PINMAP_STR "MaxSTM v3"
   #include "Pins.MaxSTM.h"
