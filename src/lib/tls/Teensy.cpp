1fbfcbcc20c131f0c52f492e71511ae96a0da1a9
Howard Dutton <hjd1964@gmail.com>
2023-07-27 09:26:02 -0400

    Fixes for Teensy RTC support

--- src/lib/tls/Teensy.cpp
+++ src/lib/tls/Teensy.cpp
@@ -14,33 +14,24 @@ bool TimeLocationSource::init() {
 
 void TimeLocationSource::set(JulianDate ut1) {
   GregorianDate greg = calendars.julianDayToGregorian(ut1);
-  greg.year -= 2000;
-  if (greg.year >= 100) greg.year -= 100;
 
-  double f1 = fabs(ut1.hour);
+  double f1 = fabs(ut1.hour) + TLS_CLOCK_SKEW;
   int h = floor(f1);
   double m = (f1 - h)*60.0;
   double s = (m - floor(m))*60.0;
 
-  set(h, floor(m), floor(s), greg.day, greg.month, greg.year);
+  set(greg.year, greg.month, greg.day, h, floor(m), floor(s));
 }
 
 void TimeLocationSource::set(int year, int month, int day, int hour, int minute, int second) {
   setTime(hour, minute, second, day, month, year);
   unsigned long TeensyTime = now();              // get time in epoch
-
-  // due to broken Teensy4.x RTC libraries, no point calling this as all it does is corrupt the possibly good time
-  #if !defined(ARDUINO_TEENSY40) && !defined(ARDUINO_TEENSY41)
-    Teensy3Clock.set(TeensyTime);                // set Teensy time
-  #endif
+  Teensy3Clock.set(TeensyTime);                  // set Teensy time
 }
 
 void TimeLocationSource::get(JulianDate &ut1) {
   unsigned long TeensyTime = Teensy3Clock.get(); // get time from Teensy RTC
-
-  // due to broken Teensy4.x RTC libraries, apply clock skew on read (where we have control) instead of write as typically done
-  // this allows "correcting" local time (as set during Sketch upload) into UTC
-  setTime(TeensyTime - TLS_CLOCK_SKEW*3600);     // set system time
+  setTime(TeensyTime);                           // set system time
 
   if (year() >= 0 && year() <= 3000 && month() >= 1 && month() <= 12 && day() >= 1 && day() <= 31 &&
       hour() <= 23 && minute() <= 59 && second() <= 59) {
