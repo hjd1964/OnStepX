b9fe623487ee57d6b70e5239386f1af655364ee6
drewbolce <drewbolce@users.noreply.github.com>
2023-08-12 09:52:43 -0400

    Update Config.h
    Added NV_driver option

--- Config.h
+++ Config.h
@@ -138,6 +138,9 @@
 #define TIME_LOCATION_PPS_SENSE       OFF //    OFF, HIGH senses PPS (pulse per second,) signal rising edge, or use LOW for   Option
                                           //         falling edge, or use BOTH for rising and falling edges.
                                           //         Better tracking accuracy especially for Mega2560's w/ceramic resonator.
+// Non-Volitale memory ------------------------------------------
+#define NV_DRIVER              NV_DEFAULT //    NV_DEFAULT, For alternate use NV_AT24C32 on DS3231 module. Use                Option
+                                          //         NV_MB85RC64 on new MaxESP4i                                                       
 
 // STATUS ------------------------------------------------------ see https://onstep.groups.io/g/main/wiki/Configuration_Mount#STATUS
 #define STATUS_MOUNT_LED              OFF //    OFF, ON Flashes proportional to rate of movement or solid on for slews.       Option
