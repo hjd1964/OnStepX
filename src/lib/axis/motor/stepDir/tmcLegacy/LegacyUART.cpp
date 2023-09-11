63e7042e6a2f430d989de7df56350f4c27127899
Howard Dutton <hjd1964@gmail.com>
2023-09-11 09:18:55 -0400

    Fix for missing driver ot status

--- src/lib/axis/motor/stepDir/tmcLegacy/LegacyUART.cpp
+++ src/lib/axis/motor/stepDir/tmcLegacy/LegacyUART.cpp
@@ -191,6 +191,7 @@ void StepDirTmcUART::updateStatus() {
       status.outputB.shortToGround = (bool)tmc2209Status.short_to_ground_b || (bool)tmc2209Status.low_side_short_b;
       status.outputB.openLoad      = (bool)tmc2209Status.open_load_b;
       status.overTemperatureWarning = (bool)tmc2209Status.over_temperature_warning;
+      status.overTemperature       = (bool)tmc2209Status.over_temperature_shutdown;
       status.standstill            = (bool)tmc2209Status.standstill;
 
       // open load indication is not reliable in standstill
