889120ea209fd8bd81f313b70ef5869f4818e797
Howard Dutton <hjd1964@gmail.com>
2023-09-14 07:28:50 -0400

    Better weather validation

--- src/libApp/weather/Weather.cpp
+++ src/libApp/weather/Weather.cpp
@@ -92,16 +92,39 @@ void Weather::poll() {
     if (success && !xBusy) {
       static int phase = 0;
       switch (++phase) {
-        case 1: temperature = bmx.readTemperature(); return; break;
-        case 2: pressure = bmx.readPressure()/100.0; return; break;
+        case 1:
+          temperature = bmx.readTemperature();
+          if (!isnan(temperature) && (temperature < -60.0F || temperature > 60.0F)) temperature = NAN;
+          return;
+        break;
+        case 2:
+          pressure = bmx.readPressure()/100.0F;
+          if (!isnan(pressure) && (pressure < 100.0F || pressure > 1100.0F)) pressure = NAN;
+          return;
+        break;
         case 3: 
           #if WEATHER == BME280 || WEATHER == BME280_0x76 || WEATHER == BME280_SPI
             humidity = bmx.readHumidity();
+            if (!isnan(humidity) && (humidity < 0.0F || humidity > 100.0F)) humidity = NAN;
           #endif
           phase = 0;
         break;
       }
       bmx.takeForcedMeasurement();
+
+      // if any measurements are anomalous assume all are invalid
+      #if WEATHER == BME280 || WEATHER == BME280_0x76 || WEATHER == BME280_SPI
+        if (isnan(temperature) || isnan(pressure) || isnan(humidity)) {
+          temperature = NAN;
+          pressure = NAN;
+          humidity = NAN;
+        }
+      #else
+        if (isnan(temperature) || isnan(pressure)) {
+          temperature = NAN;
+          pressure = NAN;
+        }
+      #endif
 
       // apply a 10 sample rolling average to the ambient temperature
       static uint8_t nanCount = 0;
@@ -113,7 +136,7 @@ void Weather::poll() {
 
       if (!isnan(temperature)) {
         nanCount = 0;
-        averageTemperature = (averageTemperature*9.0 + temperature)/10.0;
+        averageTemperature = (averageTemperature*9.0F + temperature)/10.0F;
       } else {
         if (nanCount < 15) {
           nanCount++;
@@ -139,7 +162,7 @@ float Weather::getTemperature() {
 // set temperature in deg. C
 bool Weather::setTemperature(float t) {
   if (weatherSensor == WS_NONE) { 
-    if (t >= -100.0 && t < 100.0) { temperature = t; averageTemperature = t; } else return false;
+    if (t >= -60.0F && t <= 60.0F) { temperature = t; averageTemperature = t; } else return false;
   }
   return true;
 }
@@ -152,7 +175,7 @@ float Weather::getPressure() {
 // set barometric pressure in hPa/mb
 bool Weather::setPressure(float p) {
   if (weatherSensor == WS_NONE) { 
-    if (p >= 500.0 && p < 1500.0) pressure = p; else return false;
+    if (p >= 100.0F && p <= 1100.0F) pressure = p; else return false;
   }
   return true;
 }
@@ -165,7 +188,7 @@ float Weather::getHumidity() {
 // set relative humidity in %
 bool Weather::setHumidity(float h) {
   if (weatherSensor == WS_NONE || weatherSensor == WS_BMP280) { 
-    if (h >= 0.0 && h < 100.0) humidity = h; else return false;
+    if (h >= 0.0F && h <= 100.0F) humidity = h; else return false;
   }
   return true;
 }
@@ -173,7 +196,7 @@ bool Weather::setHumidity(float h) {
 // get dew point in deg. C
 // accurate to +/- 1 deg. C for RH above 50%
 float Weather::getDewPoint() {
-  return averageTemperature - ((100.0 - humidity) / 5.0);
+  return averageTemperature - ((100.0F - humidity)/5.0F);
   // a more accurate formula?
   // return 243.04*(log(humidity/100.0) + ((17.625*averageTemperature)/(243.04 + averageTemperature)))/(17.625 - log(humidity/100.0) - ((17.625*averageTemperature)/(243.04 + averageTemperature)));
 }
