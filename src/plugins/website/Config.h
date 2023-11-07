dfb6a7734d0e83167918c4fa3295ef8817c0f455
Howard Dutton <hjd1964@gmail.com>
2023-11-07 16:17:29 -0500

    Update Config.h
    Turned weather reporting on

--- src/plugins/website/Config.h
+++ src/plugins/website/Config.h
@@ -12,7 +12,7 @@
 //      Parameter Name              Value   Default  Notes                                                                      Hint
 // DISPLAY -------------------------------------------------------------------------------------------------------------------------
 #define DISPLAY_LANGUAGE             L_en //   L_en, English. Or L_ce, L_de, L_en, L_us, L_es two letter country code.        Adjust
-#define DISPLAY_WEATHER               OFF //    OFF, ON ambient conditions in locale default units.                           Option
+#define DISPLAY_WEATHER                ON //    OFF, ON ambient conditions in locale default units.                           Option
 #define DISPLAY_INTERNAL_TEMPERATURE  OFF //    OFF, ON internal MCU temp. in locale default units.                           Option
 #define DISPLAY_WIFI_SIGNAL_STRENGTH   ON //     ON, Wireless signal strength reported via web interface. OFF otherwise.      Option
 #define DISPLAY_SERVO_MONITOR         OFF //    OFF, ON to display the servo monitor (any axis.)                              Option
