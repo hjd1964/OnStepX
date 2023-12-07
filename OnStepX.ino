e7fcd98e8a2eb4b8160670ac4c050898d32c6768
Howard Dutton <hjd1964@gmail.com>
2023-12-07 11:26:12 -0500

    Upped patch level

--- OnStepX.ino
+++ OnStepX.ino
@@ -44,7 +44,7 @@
 #define FirmwareName                "On-Step"
 #define FirmwareVersionMajor        10
 #define FirmwareVersionMinor        19     // minor version 00 to 99
-#define FirmwareVersionPatch        "o"    // for example major.minor patch: 10.03c
+#define FirmwareVersionPatch        "p"    // for example major.minor patch: 10.03c
 #define FirmwareVersionConfig       6      // internal, for tracking configuration file changes
 
 #include "src/Common.h"
