1b34df23544eb3f58989481defcdb5e0a9bdec17
Howard Dutton <hjd1964@gmail.com>
2023-12-21 14:31:28 -0500

    Fix for jtw24 encoder support

--- src/telescope/mount/Mount.axis.cpp
+++ src/telescope/mount/Mount.axis.cpp
@@ -25,6 +25,8 @@
     PulseOnly encAxis1(AXIS1_ENCODER_A_PIN, &servoControlAxis1.directionHint, 1);
   #elif AXIS1_ENCODER == AS37_H39B_B
     As37h39bb encAxis1(AXIS1_ENCODER_A_PIN, AXIS1_ENCODER_B_PIN, 1);
+  #elif AXIS1_ENCODER == JTW_24BIT
+    Jtw24 encAxis1(AXIS1_ENCODER_A_PIN, AXIS1_ENCODER_B_PIN, 1);
   #elif AXIS1_ENCODER == SERIAL_BRIDGE
     SerialBridge encAxis1(1);
   #endif
@@ -89,6 +91,8 @@ Axis axis1(1, &PinsAxis1, &SettingsAxis1, AXIS_MEASURE_RADIANS, arcsecToRad(AXIS
     PulseOnly encAxis2(AXIS2_ENCODER_A_PIN, &servoControlAxis2.directionHint, 2);
   #elif AXIS2_ENCODER == AS37_H39B_B
     As37h39bb encAxis2(AXIS2_ENCODER_A_PIN, AXIS2_ENCODER_B_PIN, 2);
+  #elif AXIS2_ENCODER == JTW_24BIT
+    Jtw24 encAxis2(AXIS2_ENCODER_A_PIN, AXIS2_ENCODER_B_PIN, 2);
   #elif AXIS2_ENCODER == SERIAL_BRIDGE
     SerialBridge encAxis2(2);
   #endif
