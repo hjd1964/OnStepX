069414a597ab768984253ca7f332059b2f258f46
Howard Dutton <hjd1964@gmail.com>
2024-04-02 16:15:05 -0400

    Better base timer rate for F401/F411

--- src/HAL/HAL_STM32F4x1.h
+++ src/HAL/HAL_STM32F4x1.h
@@ -6,8 +6,8 @@
 
 #define HAL_FAST_PROCESSOR
 
-// Base rate for critical task timing (0.0095s = 0.14", 0.2 sec/day)
-#define HAL_FRACTIONAL_SEC 105.2631579F
+// Base rate for critical task timing (0.0057s = 0.09", 0.2 sec/day)
+#define HAL_FRACTIONAL_SEC 175.4385965F
 
 // Analog read and write
 #ifndef ANALOG_READ_RANGE
