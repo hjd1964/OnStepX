5bcb0c64ac792808e2f9e8905c2962f960edc6bb
Howard Dutton <hjd1964@gmail.com>
2024-06-18 10:02:31 -0400

    Fix for destination side of pier

--- src/telescope/mount/goto/Goto.command.cpp
+++ src/telescope/mount/goto/Goto.command.cpp
@@ -255,7 +255,10 @@ bool Goto::command(char *reply, char *command, char *parameter, bool *supressFra
     //              1=destination is West of the pier
     //              2=destination is Unknown
     if (command[1] == 'D' && parameter[0] == 0) {
-      CommandError e = setTarget(&gotoTarget, settings.preferredPierSide);
+      Coordinate coords = gotoTarget;
+      coords.pierSide = PIER_SIDE_NONE;
+      transform.nativeToMount(&coords);
+      CommandError e = setTarget(&coords, settings.preferredPierSide);
       strcpy(reply, "2");
       if (e == CE_NONE && target.pierSide == PIER_SIDE_EAST) reply[0] = '0';
       if (e == CE_NONE && target.pierSide == PIER_SIDE_WEST) reply[0] = '1';
