8ebc41a1f9fb4336c8c37c8c9a3f9dc1e723b54c
Howard Dutton <hjd1964@gmail.com>
2024-11-21 06:10:42 -0500

    Added T4.1 to comment

--- src/lib/tasks/HAL_HWTIMERS.h
+++ src/lib/tasks/HAL_HWTIMERS.h
@@ -37,7 +37,7 @@
       defined(STM32F401xC) || defined(STM32F407xx) || \
       defined(STM32F303xC) || defined(STM32F103xB)
   #include "HAL_STM32_HWTIMER.h"
-// Teensy3.0, 3.1, 3.2, 3.5, 3.6, and 4.0
+// Teensy3.0, 3.1, 3.2, 3.5, 3.6, 4.0, and 4.1
 #elif defined(_mk20dx128_h_) || defined(__MK20DX128__) || \
       defined(__MK20DX256__) || defined(__MK64FX512__) || \
       defined(__MK66FX1M0__) || defined(__IMXRT1052__) || \
