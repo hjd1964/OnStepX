5aaa6ddb5d7f71680019f778553f86116259bbcb
Howard Dutton <hjd1964@gmail.com>
2025-05-02 09:14:54 -0400

    Better comments

--- src/lib/axis/motor/servo/Servo.cpp
+++ src/lib/axis/motor/servo/Servo.cpp
@@ -111,12 +111,12 @@ bool ServoMotor::init() {
   return true;
 }
 
-// set driver parameters
 void ServoMotor::setParameters(float param1, float param2, float param3, float param4, float param5, float param6) {
+// set motor parameters
   feedback->setParameters(param1, param2, param3, param4, param5, param6);
 }
 
-// validate driver parameters
+// validate motor parameters
 bool ServoMotor::validateParameters(float param1, float param2, float param3, float param4, float param5, float param6) {
   return feedback->validateParameters(param1, param2, param3, param4, param5, param6);
 }
