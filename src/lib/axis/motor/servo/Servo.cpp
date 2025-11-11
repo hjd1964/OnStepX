e78326b261f74081c35519802436ebbf4f010f3e
Panagiotis Papadakos <papadako@gmail.com>
2025-11-11 19:53:48 +0200

    typos

--- src/lib/axis/motor/servo/Servo.cpp
+++ src/lib/axis/motor/servo/Servo.cpp
@@ -68,9 +68,8 @@ bool ServoMotor::init() {
 
   #ifdef CALIBRATE_SERVO_DC
     calibrateVelocity = new ServoCalibrateTrackingVelocity(axisNumber);
-    driver->setTrackingMode(false);
+    driver->setTrackingMode(true);
     driver->setBypassAccelOnTracking(true);
-    currentFrequency = 0.0f;                    // no trajectory input
     slewing = false;
   #endif
 
